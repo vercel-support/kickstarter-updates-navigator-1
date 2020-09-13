4475d9faa05015b49e769c64a9337f7f44df58a2
João Oliveira <joao.oliveira@real-digital.de>
2020-09-13 04:46:48 +0200

    Remove publicPath adjustment needed for GitHub Pages

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,6 @@
 process.env.VUE_APP_VERSION = require('./package.json').version
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-      ? '/kickstarter-updates-navigator/'
-      : '/',
   transpileDependencies: [
     "vuetify"
   ]
